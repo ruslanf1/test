0eacb0923bce89dd149c55f85f7533d1183b9c04
ruslanf1 <62360818+ruslanf1@users.noreply.github.com>
2023-08-31 12:39:43 +0200

    Create Cart.php

---
+++ Cart.php
@@ -0,0 +1,60 @@
+<?php
+
+class Cart implements iCartInterface
+{
+    public array $items;
+    public object $order;
+
+    /**
+     * @return float|int
+     */
+    public function calcVat(): float|int
+    {
+        return $this->calc(0.18);
+    }
+
+    /**
+     * @return void
+     */
+    public function notify(): void
+    {
+        $price = $this->calc();
+        $this->sendMail($price);
+    }
+
+    /**
+     * @param float|int $discount
+     * @return void
+     */
+    public function makeOrder(float|int $discount = 1.0): void
+    {
+        $price = $this->calc() * $discount;
+        $this->order = new Order($this->items, $price);
+        $this->notify();
+    }
+
+    /**
+     * @param float|int $price
+     * @return void
+     */
+    public function sendMail(float|int $price): void
+    {
+        $message = "<p> <b>" . $this->order->id() . "</b> " . $price . " .</p>";
+
+        $mailer = new SimpleMailer('cartuser', 'j049lj-01');
+        $mailer->sendToManagers($message);
+    }
+
+    /**
+     * @param float|int $value
+     * @return float|int
+     */
+    public function calc(float|int $value = 1.18): float|int
+    {
+        $price = 0;
+        foreach ($this->items as $item) {
+            $price += $item->getPrice() * $value;
+        }
+        return $price;
+    }
+}
